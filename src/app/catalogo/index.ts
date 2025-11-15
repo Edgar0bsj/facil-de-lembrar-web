d57c483df3a03cce7bde3534e6d09e7499ae6650
Edgar0bsj <edgarbsj.dev@gmail.com>
2025-11-15 15:00:45 -0300

    :truck: reorganiza estrutura de componentes e remove arquivos obsoletos

--- src/app/catalogo/index.ts
+++ src/app/catalogo/index.ts
@@ -1,9 +1,9 @@
-import FeaturesSection from "@/components/features/FeaturesSection";
+import FeaturesSection from "@/components/feature/FeaturesSection";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
-import ProductCard from "@/components/productCard/ProductCard";
+import ProductCard from "@/components/product/ProductCard";
 import useCatalogo from "@/hooks/UseCatalogo";
-import LayoutProductCard from "@/components/productCard/LayoutProductCard";
+import LayoutProductCard from "@/components/product/LayoutProductCard";
 
 export default function useComponents() {
   const catalogo = useCatalogo;
